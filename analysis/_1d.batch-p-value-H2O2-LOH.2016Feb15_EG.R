8461909f58fea4afed7e6fd35c15ecce499ba2ce
emineguven <eguven@biolapmac03.spelman.edu>
2016-02-17 11:57:59 -0500

    HQ revision

--- analysis/_1d.batch-p-value-H2O2-LOH.2016Feb15_EG.R
+++ analysis/_1d.batch-p-value-H2O2-LOH.2016Feb15_EG.R
@@ -1,34 +1,23 @@
-#batch plot, 2016Feb, testing p-value for 
+#batch plot, 2016 Feb 16, testing p-value for 
 #if We can estimate p-value of observed ¾ blacks from ¼ and ½ black distributions. 
 #assuming multinomial distribution 
 
-# used the same script based on 2013 Dec 10, adjust for low-counts
-#tb$Black[tb$Black<=0] = 0.5
-#tb$halfBlack[tb$halfBlack<=0] = 0.5
-
-#batch plot, 2013Dec, log plot
-
-# 2013 Dec 10, adjust for low-counts
-#tb$Black[tb$Black<=0] = 0.5
-#tb$halfBlack[tb$halfBlack<=0] = 0.5
-
 rm=(list=ls())
-#setwd("~/projects/LOH-oxidants2012.osX/analysis")
 setwd("~/github/LOH_H2O2_2016/analysis")
 debug = 0;
 
-
 FileList = list.files( path="../data.H2O2-LOH/");  FileList; 
-AllpValues=list()
-if( debug > 5) {FileList = FileList[1:2]}
+output = data.frame(FileList)
+output$p_ttest = NA; 
 
-for( infile in FileList) {
-  
-  # infile='M1-2,20111207.modified.csv' #debug for NA, and low lead concentration
-  # infile =  'M2-8,08172011.H2O2.LOH.modified.csv';  #viability plot error
-  #infile =  'M2-8,06062011.H2O2onLOH.csv';  #b plot error
+if( debug > 5) {FileList = FileList[1:6]}
+
+for( ii in 1:length(FileList)) {  
+  infile = FileList[[ii]] #for list
+  print( paste("ii= ", ii ))
   
   fullFileName = paste('../data.H2O2-LOH/',infile, sep='');
+  print( fullFileName )
   mylabel = infile
   
   tb = read.csv(fullFileName, colClasses=c("character",NA, NA, "character", rep("numeric",8 ), NA));
@@ -54,10 +43,6 @@ for( infile in FileList) {
   
   tb$Dilution = tb$Dilution / tb$Dilution[1]
   
-  ###### some manual curations here
-  #tb$Dilution[c(19,20,21)] = c(10,10,10)  ##this dilution should be 10 times more
-  #tb$Black[25] = NA #This number is not right!!!!
-  
   ######## normalize all data
   mycolumns = c("White","Black","halfBlack", "quarterBlack","ThreeQBlack", "QQBlack", "Other","tot"); 
   for ( j in mycolumns) {
@@ -70,7 +55,7 @@ for( infile in FileList) {
   tbm = data.frame(cbind(H2O2))
   for ( i in 1:length(H2O2)) {
     c = H2O2[i]
-    tmp = tb[ tb$H2O2==c, ]	
+    tmp = tb[ tb$H2O2==c, ]  
     tbm$tot[i] = mean(tmp$tot, na.rm=T)
     tbm$White[i] = mean(tmp$White, na.rm=T)
     tbm$Black[i] = mean(tmp$Black, na.rm=T) 
@@ -82,9 +67,6 @@ for( infile in FileList) {
   
   tbm = tbm[tbm$tot>1, ] #remove plates with zero colonies
   
-  ###### some manual curations here
-  #tbm$halfBlack[tbm$halfBlack==0 & tbm$H2O2>0] = NA;
-  
   ###### calculate fractions
   tbf = tbm; 
   tbf$s = tbf$tot / max(tbf$tot)
@@ -94,28 +76,20 @@ for( infile in FileList) {
   
   tbf$Black[tbf$Black<0]=NA;  #remove weird experimental data, such as low-lead concentration effect
   
-  TQB<-tbf$ThreeQBlack
-  normalized = (TQB-min(TQB))/(max(TQB)-min(TQB))
-  HQB<-tbf$halfBlack*tbf$quarterBlack
-  normalized2 = (HQB-min(HQB))/(max(HQB)-min(HQB))
+  # TQB<-tbf$ThreeQBlack
+  # normalized = (TQB-min(TQB))/(max(TQB)-min(TQB)) #Qin does not understand this line. 
+  H0TQB<-tbf$halfBlack*tbf$quarterBlack
+  # normalized2 = (HQB-min(HQB))/(max(HQB)-min(HQB)) #Qin does not understand this line
   #find the standard error of multiplication
-  std_HQB<-sqrt(var(HQB)/length(HQB))
+  #this is normalized between [-1,1]
   
-  #Histogram of  normalized data
-  par(mfrow=c(1,3))
-  hist(normalized,col="lightblue",xlab="ThreeQBlack",main="")
-  hist(normalized2,col="magenta",xlab="1/2*1/4",main="")
-  #histogram of joint data
-  hist(normalized,breaks=20,freq=F,col="lightblue",xlab="joint",main="")
-  #Histogram of normalized data
-  hist(normalized2,breaks=20,freq=F,col="magenta",add=T)
+  tt = t.test( tbf$ThreeQBlack, H0TQB, pairwise=T, alternative = "greater")
+  print(tt)
+  output$p_ttest[ii] = tt$p.value
   
- 
-  
-  pvalue<-t.test(normalized2)$p.value
-  AllpValues[[length(AllpValues)+1]] = pvalue;
-  
-  
-  
+  #pvalue<-t.test(normalized2)$p.value
+  #AllpValues[[length(AllpValues)+1]] = pvalue;
 }
 
+head(output)
+write.csv(output, "__batch_ttest-threeQBlack.csv")
